a8d27134a8d806f16cf21fbc65723f0a50bcc0da
thessias <slavka2606@gmail.com>
2021-01-26 13:15:05 +0100

    Minor fixes

--- SaneLogGenerator/Options.cs
+++ SaneLogGenerator/Options.cs
@@ -8,37 +8,37 @@ namespace SaneLogGenerator
 {
     internal class Options
     {
-        [Option('e', "events", Required = true, HelpText = "Total number of events in log.")]
+        [Option('e', "events", Required = true, HelpText = "Total number of events in log. (int)")]
         public int NumberOfEvents { get; set; }
 
-        [Option('v', "variants", Required = true, HelpText = "Total number of variants used in log.")]
+        [Option('v', "variants", Required = true, HelpText = "Total number of variants used in log. (int)")]
         public int NumberOfVariants { get; set; }
 
-        [Option('x', "min-activities-per-case", Required = true, HelpText = "Min number of activities per case.")]
+        [Option('x', "min-activities-per-case", Required = true, HelpText = "Min number of activities per case. (int)")]
         public int MinActivitiesPerCase { get; set; }
 
-        [Option('y', "max-aActivities-per-case", Required = true, HelpText = "Max number of activities per case.")]
+        [Option('y', "max-activities-per-case", Required = true, HelpText = "Max number of activities per case. (int)")]
         public int MaxActivitiesPerCase { get; set; }
 
-        [Option('a', "activities", Required = true, HelpText = "Number of different activities used in log.")]
+        [Option('a', "activities", Required = true, HelpText = "Number of different activities used in log. (int)")]
         public int NumberOfActivities { get; set; }
 
-        [Option('r', "resources", Required = true, HelpText = "Number of different resources used in log.")]
+        [Option('r', "resources", Required = true, HelpText = "Number of different resources used in log. (int)")]
         public int NumberOfResources { get; set; }
 
         [Option('d', "start-datetime", Required = true, HelpText = "Start DateTime used in log.")]
         public DateTime StartDateTime { get; set; }
 
-        [Option('p', "parallel", Default = false, HelpText = "Parallel activities used. True/False")]
+        [Option('p', "parallel", Default = false, HelpText = "Parallel activities in log. (no value needed)")]
         public bool Parallel { get; set; }
 
-        [Option('s', "specialChars", Default = false, HelpText = "Special characters used. True/False")]
+        [Option('s', "special-chars", Default = false, HelpText = "Special characters used in log. (no value needed)")]
         public bool SpecialChars { get; set; }
 
-        [Option('n', "empty-fileds", Default = false, HelpText = "Some fileds will be empty. True/False")]
-        public bool EmptyField { get; set; }
+        // [Option('n', "empty-fileds", Default = false, HelpText = "Some fileds will be empty.")]
+        // public bool EmptyField { get; set; }
 
-        [Option('f', "path-to-file", Default = @"C:\SLG_out.txt", HelpText = "Path to output file.")]
+        [Option('f', "path-to-file", Required = true, HelpText = "Path to output file.")]
         public string PathToFile { get; set; }
 
         public static string GetUsage()
--- SaneLogGenerator/Program.cs
+++ SaneLogGenerator/Program.cs
@@ -14,6 +14,8 @@ namespace SaneLogGenerator
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("Starting...");
+
             int numberOfEvents = 0;
             int numberOfVariants = 0;
             int minActivitiesPerCase = 0;
@@ -24,7 +26,7 @@ namespace SaneLogGenerator
 
             bool parallel = false;
             bool specialChars = false;
-            bool emptyFields = false;
+            //bool emptyFields = false;
 
             string pathToFile = string.Empty;
 
@@ -32,6 +34,9 @@ namespace SaneLogGenerator
             Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
             {
                 numberOfEvents = o.NumberOfEvents;
+                if (numberOfEvents.Equals(0))
+                {
+                }
                 numberOfVariants = o.NumberOfVariants;
                 minActivitiesPerCase = o.MinActivitiesPerCase;
                 maxActivitiesPerCase = o.MaxActivitiesPerCase;
@@ -41,22 +46,46 @@ namespace SaneLogGenerator
 
                 parallel = o.Parallel;
                 specialChars = o.SpecialChars;
-                emptyFields = o.EmptyField;
-
+                //emptyFields = o.EmptyField;
                 pathToFile = o.PathToFile;
             }
             );
 
-            List<string> resources1 = DataGenerator.GenerateResources(numberOfResources, specialChars, 1);
-            List<string> resources2 = DataGenerator.GenerateResources(numberOfResources, specialChars, 2);
-            List<string> activities = DataGenerator.GenerateActivities(numberOfActivities, specialChars);
+            bool isPathToFileEmpty = string.IsNullOrEmpty(pathToFile);
+            if (isPathToFileEmpty.Equals(false))
+                try
+                {
+                    Console.WriteLine("Total number of events: " + numberOfEvents);
+                    Console.WriteLine("Total number of variants: " + numberOfVariants);
+                    Console.WriteLine("Min number of activities per case: " + minActivitiesPerCase);
+                    Console.WriteLine("Max number of activities per case: " + maxActivitiesPerCase);
+                    Console.WriteLine("Number of activities: " + numberOfActivities);
+                    Console.WriteLine("Number of resources: " + numberOfResources);
+                    Console.WriteLine("Start DateTime: " + startDateTime);
+                    Console.WriteLine("Parallel activities present: " + parallel);
+                    Console.WriteLine("Special characters present: " + specialChars);
+                    Console.WriteLine("Path to output file: " + pathToFile);
 
-            Config config = new Config(numberOfEvents, numberOfVariants, minActivitiesPerCase, maxActivitiesPerCase,
-                                          numberOfActivities, numberOfResources, startDateTime, specialChars, parallel, pathToFile,
-                                           activities, resources1, resources2);
-            List<List<string>> variants = DataGenerator.GenerateVariants(config);
+                    List<string> resources1 = DataGenerator.GenerateResources(numberOfResources, specialChars, 1);
+                    List<string> resources2 = DataGenerator.GenerateResources(numberOfResources, specialChars, 2);
+                    List<string> activities = DataGenerator.GenerateActivities(numberOfActivities, specialChars);
 
-            WriterToCsv.WriteEventsToCsv(config, variants);
+                    Config config = new Config(numberOfEvents, numberOfVariants, minActivitiesPerCase, maxActivitiesPerCase,
+                                                  numberOfActivities, numberOfResources, startDateTime, specialChars, parallel, pathToFile,
+                                                   activities, resources1, resources2);
+                    List<List<string>> variants = DataGenerator.GenerateVariants(config);
+
+                    WriterToCsv.WriteEventsToCsv(config, variants);
+                    Console.WriteLine("Finished.");
+                    Console.WriteLine("Press any key to close.");
+
+                    Console.Read();
+                }
+                catch
+                {
+                    Console.Read();
+                    return;
+                }
         }
     }
 }
